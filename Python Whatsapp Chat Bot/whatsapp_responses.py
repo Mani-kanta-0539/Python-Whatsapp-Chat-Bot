f93d39d6601474216f21010769e7e950f4505aec
Mani_kanta_359 <112759657+Mani-kanta-0539@users.noreply.github.com>
2023-01-10 19:00:45 +0530

    Add files via upload

---
+++ Python Whatsapp Chat Bot/whatsapp_responses.py
@@ -0,0 +1,9 @@
+def response(input_message):
+    message = input_message.lower()
+
+    if message == 'nice':
+        return 'Very nice'
+    elif message  == 'hello':
+        return 'hello there'
+    else: 
+        return 'cool!'
